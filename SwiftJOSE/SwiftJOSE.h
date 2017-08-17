2e0ef1a00aafc517658dbe8fa85e3403a54f4ec8
Daniel <daniel.egger@mohemian.com>
2017-08-17 15:10:58 +0200

    [JWS-2] Add empty Xcode project

---
+++ SwiftJOSE/SwiftJOSE.h
@@ -0,0 +1,19 @@
+//
+//  SwiftJOSE.h
+//  SwiftJOSE
+//
+//  Created by Daniel Egger on 17/08/2017.
+//  Copyright © 2017 Airside Mobile, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftJOSE.
+FOUNDATION_EXPORT double SwiftJOSEVersionNumber;
+
+//! Project version string for SwiftJOSE.
+FOUNDATION_EXPORT const unsigned char SwiftJOSEVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftJOSE/PublicHeader.h>
+
+
