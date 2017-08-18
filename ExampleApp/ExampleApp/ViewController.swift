0e46bb2d39f240da00ae134eda276720f363ceb2
Daniel <daniel-mohemian@users.noreply.github.com>
2017-08-18 09:41:02 +0200

    [JWS-2] Setup empty Xcode project (#1)
    * [JWS-2] Add empty Xcode project
    
    * [JWS-2] Restructure project
    
    * [JWS-2] Setup example (test) app
    
    * [JWS-2] Remove link to example app from framework
    
    * [JWS-2] Update readme

---
+++ ExampleApp/ExampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExampleApp
+//
+//  Created by Daniel Egger on 17/08/2017.
+//  Copyright © 2017 Airside Mobile, Inc. All rights reserved.
+//
+
+import UIKit
+import SwiftJOSE
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
