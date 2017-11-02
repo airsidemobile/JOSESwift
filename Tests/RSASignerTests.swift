20bae2495d001d2b02c931a5382b8dbfc0286f95
Carol Capek <carol.capek@mohemian.com>
2017-11-02 10:29:21 +0100

    [JOSE-23] Add RSASignerTests file

---
+++ Tests/RSASignerTests.swift
@@ -0,0 +1,21 @@
+//
+//  RSASignerTests.swift
+//  Tests
+//
+//  Created by Carol Capek on 02.11.17.
+//  Copyright © 2017 Airside Mobile, Inc. All rights reserved.
+//
+
+import XCTest
+
+class RSASignerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+}
