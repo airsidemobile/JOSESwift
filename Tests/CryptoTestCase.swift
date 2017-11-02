84d26bd22bf601a3f5d0612d4ec2f47f2025a83f
Carol Capek <carol.capek@mohemian.com>
2017-11-02 17:40:04 +0100

    [JOSE-23] Add cryptotestcase as super class for all crypto related stuff and retrieve private key from plist file

---
+++ Tests/CryptoTestCase.swift
@@ -0,0 +1,51 @@
+//
+//  CryptoTestCase.swift
+//  Tests
+//
+//  Created by Carol Capek on 02.11.17.
+//  Copyright © 2017 Airside Mobile, Inc. All rights reserved.
+//
+
+import XCTest
+
+class CryptoTestCase: XCTestCase {
+    
+    let privateKeyTag = "com.airsidemobile.SwiftJOSE.testPrivateKey"
+    var privateKey: SecKey? = nil
+    var publicKey: SecKey? = nil
+    
+    override func setUp() {
+        super.setUp()
+        setupKeyPair()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    private func setupKeyPair() {
+        if let path = Bundle(for: type(of: self)).path(forResource: "TestKey", ofType: "plist"), let keyDict = NSDictionary(contentsOfFile: path) {
+            
+            let keyData = Data(base64Encoded: keyDict["privateKeyBase64String"] as! String)!
+            
+            let attributes: [String: Any] = [
+                kSecAttrKeyType as String: kSecAttrKeyTypeRSA,
+                kSecAttrKeyClass as String: kSecAttrKeyClassPrivate,
+                kSecAttrKeySizeInBits as String: 2048,
+                kSecPrivateKeyAttrs as String: [
+                    kSecAttrIsPermanent as String: false,
+                    kSecAttrApplicationTag as String: privateKeyTag
+                ]
+            ]
+
+            var error: Unmanaged<CFError>?
+            guard let key = SecKeyCreateWithData(keyData as CFData, attributes as CFDictionary, &error) else {
+                print(error!)
+                return
+            }
+            
+            privateKey = key
+            publicKey = SecKeyCopyPublicKey(key)
+        }
+    }
+}
