62751b0b3753c2e8eb727b818b01c73a32444c1f
Carol Capek <carol.capek@mohemian.com>
2017-11-03 13:30:14 +0100

    [JOSE-23] Add message and test compacteSerializedJWS to CryptoTestCase

--- Tests/CryptoTestCase.swift
+++ Tests/CryptoTestCase.swift
@@ -9,10 +9,12 @@
 import XCTest
 
 class CryptoTestCase: XCTestCase {
-    
+    let message = "The true sign of intelligence is not knowledge but imagination."
     let privateKeyTag = "com.airsidemobile.SwiftJOSE.testPrivateKey"
     var privateKey: SecKey? = nil
     var publicKey: SecKey? = nil
+    
+    let compactSerializedJWSConst = "eyJhbGciOiJSUzUxMiJ9.VGhlIHRydWUgc2lnbiBvZiBpbnRlbGxpZ2VuY2UgaXMgbm90IGtub3dsZWRnZSBidXQgaW1hZ2luYXRpb24u.dar4uQfhg7HpAXDrFJEP3T6cPePUIstu3tCLiz-HBEx1yAQXxLweQrKOYvIWOlt_HfxjjhxfGDoSXjnQMVHZTJaAYFNtK382pfOKpJAxE6UvkhLtvS-A6BKLWMS_aUVgqizOIXH0IeuVz1COpSLlsQ5KICUaqsxYyPfD28vbbQ9IfJ4RyJmSqEEx-M8BY2r4v_HHL-kyvjqGbSoF7o9Z6Cg1CetPJ5OHPBMXZa_Aj3LkNWn1GSw5B4WQueb8E0uJVAzLSNbxA-ZNowlOgDtKHOEkwbZu6zj7WvLEm8xovgmAha_y7HssoXnH26Nu-8RMUYw-LXUJz6Fny1F_xcv_TA"
     
     override func setUp() {
         super.setUp()
