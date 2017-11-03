3add82bd55ce1d51e717bdfb47eb91bc9058c614
Carol Capek <carol.capek@mohemian.com>
2017-11-03 13:33:47 +0100

    [JOSE-23] Check optional private and public key

--- Tests/JWSTests.swift
+++ Tests/JWSTests.swift
@@ -36,7 +36,12 @@ class JWSTests: CryptoTestCase {
     func testDeserializeFromCompactSerialization() {
         let compactSerializedJWS = "eyJhbGciOiJSUzUxMiJ9.SGVsbG8gd29ybGQh.UlM1MTIoZXlKaGJHY2lPaUpTVXpVeE1pSjkuU0dWc2JHOGdkMjl5YkdRaCk"
         
-        let jws = JWS(compactSerialization: compactSerializedJWS)
-        XCTAssertEqual(jws.description, "[\"alg\": RS512] . Hello world! . RS512(eyJhbGciOiJSUzUxMiJ9.SGVsbG8gd29ybGQh)")
+        let jws = JWS(compactSerialization: compactSerializedJWSConst)
+        XCTAssertEqual(String(data: jws.header.data(), encoding: .utf8), "{\"alg\":\"RS512\"}")
+        XCTAssertEqual(String(data: jws.payload.data(), encoding: .utf8), "The true sign of intelligence is not knowledge but imagination.")
+        
+        let signer = RSASigner(key: privateKey!)
+        let signature = Signature(from: signer, using: JWSHeader(algorithm: .RS512), and: JWSPayload(message.data(using: .utf8)!))
+        XCTAssertEqual(jws.signature.data(), signature?.data())
     }
 }
