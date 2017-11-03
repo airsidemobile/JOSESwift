13fe97baf96f50450c611fb9750259ba5f8188c5
Carol Capek <carol.capek@mohemian.com>
2017-11-03 13:35:11 +0100

    [JOSE-23] Check optional keys and test signing

--- Tests/RSASignerTests.swift
+++ Tests/RSASignerTests.swift
@@ -10,6 +10,7 @@ import XCTest
 @testable import SwiftJOSE
 
 class RSASignerTests: CryptoTestCase {
+    let signatureBase64URL = "Zs9rmMw-za1uXpUS2VIOcEHaMuzQl6fBCi_40kRVIE0GUruWSvpHro1oXhGwf7HqKPLx_LM8bLPCORWi9OWU4swZHY8p-GR5rhLLs2XkdIvI5kdbikr7pZOsC9NaxJKMWAntKbTZY6exkoU8vM6xL9MQtH8QFLXTjI-ZvAXbp2Ws9CnIcvOPFqAupuUKADFRpSlODlsXy71CJ3iQeBaPfHvLk61jdW6hgHYj-0WYmrFhiF1dI9MZf9J3ApdKFsW0WFuxa8Y47HlCirEOb3lz7vm8o9lNBTnt0dpWOZMTU6lHuTBgiXvyENzJoKJymVsUbMmy-2LNejmVpPt1Pm3zrA"
     
     override func setUp() {
         super.setUp()
@@ -20,9 +21,14 @@ class RSASignerTests: CryptoTestCase {
     }
     
     func testSigning() {
-        print(publicKey)
-        print(privateKey)
-        let signer = RSASigner(key: "str")
-    }
+        guard privateKey != nil else {
+            XCTFail()
+            return
+        }
     
+        let signer = RSASigner(key: privateKey!)
+        let signature = signer.sign(message.data(using: .utf8)!, using: .RS512)
+        
+        XCTAssertEqual(signature?.base64URLEncodedString(), signatureBase64URL)
+    }
 }
