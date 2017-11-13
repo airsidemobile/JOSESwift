7f1816d6a75cc52cc020da3c404d773b9704b540
carol-mohemian <carol.capek@mohemian.com>
2017-11-13 14:51:18 +0100

    [JOSE-42] Implement RS512 verifier (#15)
    * [JOSE-52] Add Algorithm extension to provide the secKeyAlgorithm and verify if the algorithm is supported
    
    * [JOSE-52] Check if algorithm is supported in sign method
    
    * [JOSE-52] Use short syntax for SecKeyAlgorithm
    
    * [JOSE-52] Set extension fileprivate
    
    * [JOSE-52] Use secKey instead of string for the key property
    
    * [JOSE-52] Use message not digest algorithm
    
    * [JOSE-41] Add public/private key pair setup method
    
    * [JOSE-41] Use public/private key pair instead of string keys
    
    * [JOSE-41] Check if algorithm is supported
    
    * [JOSE-41] Sign the singingInput and return the signature
    
    * [JOSE-41] Enable RSASignerTests
    
    * [JOSE-42] Enable JWS and RSAVerifier tests
    
    * [JOSE-52] Add secKeyAlgorithm to algorithm enum
    
    * [JOSE-52] Remove algorithm extension from RSASigner
    
    * [JOSE-41] Add SecKeyAlgorithmSupported check and use new algorithm variable
    
    * [JOSE-42] Check algorithm.secKeyAlgorithm and if it is supported to verify the signature
    
    * [JOSE-42] Verify the signature
    
    * [JOSE-42] Enable tests

--- SwiftJOSE/Sources/RSAVerifier.swift
+++ SwiftJOSE/Sources/RSAVerifier.swift
@@ -11,16 +11,19 @@ import Foundation
 public struct RSAVerifier: Verifier {
     let key: SecKey
     
-    var supportedAlgorithms: [Algorithm] {
-        return [ .RS512 ]
-    }
-    
     public init(key: SecKey) {
         self.key = key
     }
     
     public func verify(_ signature: Data, against signingInput: Data, using algorithm: Algorithm) -> Bool {
-        guard supportedAlgorithms.contains(algorithm) else {
+        guard let algorithm = algorithm.secKeyAlgorithm, SecKeyIsAlgorithmSupported(key, .verify, algorithm) else {
+            return false
+        }
+        
+        var verifyError: Unmanaged<CFError>?
+        guard SecKeyVerifySignature(key, algorithm, signingInput as CFData, signature as CFData, &verifyError) else {
+            //TODO: throw verify error
+            print("\(verifyError!)")
             return false
         }
         
