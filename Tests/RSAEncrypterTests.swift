3ad46d38a852bed736dad6261f45e610b36d26a3
Carol Capek <carol.capek@mohemian.com>
2017-11-22 12:07:29 +0100

    [JOSE-68] Add RSAEncrypter encrypting test and test cipertext

--- Tests/RSAEncrypterTests.swift
+++ Tests/RSAEncrypterTests.swift
@@ -10,6 +10,8 @@ import XCTest
 @testable import SwiftJOSE
 
 class RSAEncrypterTests: CryptoTestCase {
+    let cipherTextBase64URL = "RmKR9kGcxuf6OHkbykkPMeMiYyakVg9EtpVmZtVlQSq8EKCKmIvBd7Vw8dPu-0XpcVpKPyzSjQFRhvPBJd_dDVQKShjSzw-SB7_zfA8COo4MqAwkUEOrTEmZCsE1VdQ3dKK3sc5OoYlzOLrf8I4v4gUzXHajiBxTWcvQY3plkdfOb6Tqe9ff7FDbpO-7iCJxn3UkDVVwS3AHu15PboVQ88WOIMhVAwj5NaeWKtWw3kPfdFaaqoisbSBncdl1l7ZOYrUHGLow9QjVnS2U8CmlkNnOsHOLB6C89Jz5HsmsAzneiYlYbjKR7FL8I2ooW1CNwB_eAVTwrJIYJrvBrF8LVA"
+    
     
     override func setUp() {
         super.setUp()
@@ -19,5 +21,15 @@ class RSAEncrypterTests: CryptoTestCase {
         super.tearDown()
     }
     
-    
+    func testEncrypting() {
+        guard publicKey != nil else {
+            XCTFail()
+            return
+        }
+        
+        let encrypter = RSAEncrypter(publicKey: publicKey!)
+        let cipherText = encrypter.encrypt(message.data(using: .utf8)!)
+        
+        XCTAssertEqual(cipherText, cipherTextBase64URL.data(using: .utf8))
+    }
 }
