ef0dfe6707bb54e934b22508ef5e72c065e0f647
Carol Capek <carol.capek@mohemian.com>
2017-11-23 11:45:01 +0100

    [JOSE-68] Add encryption test

--- Tests/RSAEncrypterTests.swift
+++ Tests/RSAEncrypterTests.swift
@@ -10,9 +10,6 @@ import XCTest
 @testable import SwiftJOSE
 
 class RSAEncrypterTests: CryptoTestCase {
-    let cipherTextBase64URL = "RmKR9kGcxuf6OHkbykkPMeMiYyakVg9EtpVmZtVlQSq8EKCKmIvBd7Vw8dPu-0XpcVpKPyzSjQFRhvPBJd_dDVQKShjSzw-SB7_zfA8COo4MqAwkUEOrTEmZCsE1VdQ3dKK3sc5OoYlzOLrf8I4v4gUzXHajiBxTWcvQY3plkdfOb6Tqe9ff7FDbpO-7iCJxn3UkDVVwS3AHu15PboVQ88WOIMhVAwj5NaeWKtWw3kPfdFaaqoisbSBncdl1l7ZOYrUHGLow9QjVnS2U8CmlkNnOsHOLB6C89Jz5HsmsAzneiYlYbjKR7FL8I2ooW1CNwB_eAVTwrJIYJrvBrF8LVA"
-    
-    
     override func setUp() {
         super.setUp()
     }
@@ -22,14 +19,25 @@ class RSAEncrypterTests: CryptoTestCase {
     }
     
     func testEncrypting() {
-        guard publicKey != nil else {
+        guard publicKey != nil, privateKey != nil else {
             XCTFail()
             return
         }
         
         let encrypter = RSAEncrypter(publicKey: publicKey!)
-        let cipherText = encrypter.encrypt(message.data(using: .utf8)!)
+        guard let cipherText = encrypter.encrypt(message.data(using: .utf8)!, using: .RSAPKCS) else {
+            XCTFail()
+            return
+        }
         
-        XCTAssertEqual(cipherText, cipherTextBase64URL.data(using: .utf8))
+        var decryptionError: Unmanaged<CFError>?
+        guard let plainTextData = SecKeyCreateDecryptedData(privateKey!, .rsaEncryptionPKCS1, cipherText as CFData, &decryptionError) else {
+            XCTFail()
+            return
+        }
+        
+        XCTAssertEqual(String(data: plainTextData as Data, encoding: .utf8), message)
+    }
+        
     }
 }
