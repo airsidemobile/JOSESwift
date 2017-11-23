31de1eccdf210a7d742aefe58a35c1e750007a03
Carol Capek <carol.capek@mohemian.com>
2017-11-23 15:57:27 +0100

    [JOSE-68] Add decrypting test method

--- Tests/RSADecrypterTests.swift
+++ Tests/RSADecrypterTests.swift
@@ -10,6 +10,8 @@ import XCTest
 @testable import SwiftJOSE
 
 class RSADecrypterTests: CryptoTestCase {
+    let cipherTextBase64URL = "YkzUN55RBG1igsrL-7ofPPWruTjxNMS3Bl1_a3i-dVKBjiN6kH88j8G3iB3eLWFxbiKZvx0LmkP7J65-frpOgF41SlltjJ62LEZOICm4q21Q4MNqL_Vf7kIjh8DziEJkElEz5W4flhI6YQ-9wW_PQ-coBIPIZiFlw6peKAolz8xcevbUmnqIH6A3hOFLK23J2cWDSWgxHEBIYtZ6whQCJYL4vq5lAFNaEoDaE_cgL6LItY4t-vR1exTJSOlCGAv4uM1Kelk6uitaFk2c0h79u3UpFN_wa02m_PPdgTguRdxwRsCpsQhOKmEakl8LR6NTbIrdB13UoL2tdybltVeUCw"
+    
     
     override func setUp() {
         super.setUp()
@@ -18,4 +20,17 @@ class RSADecrypterTests: CryptoTestCase {
     override func tearDown() {
         super.tearDown()
     }
+    
+    func testDecrypting() {
+        guard privateKey != nil else {
+            XCTFail()
+            return
+        }
+        
+        let decrypter = RSADecrypter(privateKey: privateKey!)
+        let plainText = try! decrypter.decrypt(Data(base64URLEncoded: cipherTextBase64URL)!)
+        
+        XCTAssertEqual(plainText, message.data(using: .utf8))
+    }
+    
 }
