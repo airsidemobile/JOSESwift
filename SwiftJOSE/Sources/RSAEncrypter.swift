efc380da3a0b0a225ede782ad3d31c42e6e71b9e
Carol Capek <carol.capek@mohemian.com>
2017-11-23 11:45:56 +0100

    [JOSE-68] Implement RSAEncrypter encrypt method

--- SwiftJOSE/Sources/RSAEncrypter.swift
+++ SwiftJOSE/Sources/RSAEncrypter.swift
@@ -11,7 +11,28 @@ import Foundation
 public struct RSAEncrypter: AsymmetricEncrypter {
     let publicKey: SecKey
     
-    func encrypt(_ plaintext: Data) -> Data {
-        return "encryptedKey".data(using: .utf8)!
+    init(publicKey: SecKey) {
+        self.publicKey = publicKey
+    }
+    
+    func encrypt(_ plaintext: Data, using algorithm: AsymmetricEncryptionAlgorithm) -> Data? {
+        guard let algorithm = algorithm.secKeyAlgorithm, SecKeyIsAlgorithmSupported(publicKey, .encrypt, algorithm) else {
+            //TODO: Error handling
+            return nil
+        }
+        
+        guard (plaintext.count < (SecKeyGetBlockSize(publicKey) - 11)) else {
+            //TODO: Think of adding this to the `AsymmetricEncryptionAlgorithm` enum
+            //TODO: Error handling
+            return nil
+        }
+        
+        var encryptionError: Unmanaged<CFError>?
+        guard let cipherText = SecKeyCreateEncryptedData(publicKey, algorithm, plaintext as CFData, &encryptionError) else {
+            //TODO: Error handling
+            return nil
+        }
+        
+        return cipherText as Data
     }
 }
