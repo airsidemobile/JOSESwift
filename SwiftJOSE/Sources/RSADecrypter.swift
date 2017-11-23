b48ec9eb12a246bea1ce751b8137d1fa1dbafe2c
Carol Capek <carol.capek@mohemian.com>
2017-11-23 16:51:57 +0100

    [JOSE-68] Implement decryption method

--- SwiftJOSE/Sources/RSADecrypter.swift
+++ SwiftJOSE/Sources/RSADecrypter.swift
@@ -11,8 +11,20 @@ import Foundation
 public struct RSADecrypter: AsymmetricDecrypter {
     let privateKey: SecKey
     
-    func decrypt(_ ciphertext: Data) throws -> Data {
-        // Todo: Throw error if necessary.
-        return "decryptedKey".data(using: .utf8)!
+    func decrypt(_ ciphertext: Data, using algorithm: AsymmetricEncryptionAlgorithm) throws -> Data {
+        guard let algorithm = algorithm.secKeyAlgorithm, SecKeyIsAlgorithmSupported(privateKey, .decrypt, algorithm) else {
+            throw EncryptionError.keyEncryptionAlgorithmNotSupported
+        }
+        
+        guard ciphertext.count == SecKeyGetBlockSize(privateKey) else {
+            throw EncryptionError.plainTextLengthNotSatisfied
+        }
+        
+        var decryptionError: Unmanaged<CFError>?
+        guard let plainText = SecKeyCreateDecryptedData(privateKey, algorithm, ciphertext as CFData, &decryptionError) else {
+            throw EncryptionError.encryptingFailed(description: decryptionError?.takeRetainedValue().localizedDescription ?? "No description available.")
+        }
+        
+        return plainText as Data
     }
 }
