0e0ecba41fd849390ae90aecee2c758688252800
Carol Capek <carol.capek@mohemian.com>
2017-11-23 15:41:58 +0100

    [JOSE-68] Add RSADecrypterTests file

---
+++ Tests/RSADecrypterTests.swift
@@ -0,0 +1,21 @@
+//
+//  RSADecrypterTests.swift
+//  Tests
+//
+//  Created by Carol Capek on 23.11.17.
+//  Copyright © 2017 Airside Mobile, Inc. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftJOSE
+
+class RSADecrypterTests: CryptoTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+}
