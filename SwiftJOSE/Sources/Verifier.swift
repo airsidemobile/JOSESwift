716ad4b51cb2bdd01e17404e098266010b3c6e21
Carol Capek <carol.capek@mohemian.com>
2017-11-27 14:35:38 +0100

    [JOSE-68] Add Signer and Verifier docu

--- SwiftJOSE/Sources/Verifier.swift
+++ SwiftJOSE/Sources/Verifier.swift
@@ -8,6 +8,17 @@
 import Foundation
 
 public protocol Verifier {
+    /// Initializes a `Verifier` with the specified key.
     init(key: SecKey)
+    
+    /**
+     Verifies a signature against a given signing input with a specific algorithm and the corresponding key.
+     - Parameters:
+        - signature: The signature to verify.
+        - signingInput: The input to verify against.
+        - algorithm: The algorithm with which the signature was created.
+     
+     - Returns: True if the signature is verified, false if it is not verified.
+     */
     func verify(_ signature: Data, against signingInput: Data, using algorithm: SigningAlgorithm) throws -> Bool
 }
