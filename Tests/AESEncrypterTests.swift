85eeda7545b460daa3d5f67a506b7878eabd497b
Carol Capek <carol.capek@mohemian.com>
2017-11-28 14:00:41 +0100

    [JOSE-67] Add AESEncrypterTests file

---
+++ Tests/AESEncrypterTests.swift
@@ -0,0 +1,19 @@
+//
+//  AESEncrypterTests.swift
+//  Tests
+//
+//  Created by Carol Capek on 28.11.17.
+//
+
+import XCTest
+
+class AESEncrypterTests: CryptoTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+}
