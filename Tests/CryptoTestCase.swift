cd829b2093974bce94f7d3b22ef2cde176fca6a3
Carol Capek <carol.capek@mohemian.com>
2017-11-30 16:11:52 +0100

    [JOSE-67] Add hexadecimal convertion methods to convert hexstring to data and vice versa

--- Tests/CryptoTestCase.swift
+++ Tests/CryptoTestCase.swift
@@ -47,3 +47,29 @@ class CryptoTestCase: XCTestCase {
         }
     }
 }
+
+extension String {
+    
+    func hexadecimalToData() -> Data? {
+        var data = Data(capacity: count / 2)
+        
+        let regex = try! NSRegularExpression(pattern: "[0-9a-f]{1,2}", options: .caseInsensitive)
+        regex.enumerateMatches(in: self, range: NSMakeRange(0, utf16.count)) { match, flags, stop in
+            let byteString = (self as NSString).substring(with: match!.range)
+            var num = UInt8(byteString, radix: 16)!
+            data.append(&num, count: 1)
+        }
+        
+        guard data.count > 0 else { return nil }
+        
+        return data
+    }
+
+}
+
+extension Data {
+    func toHexadecimal() -> String {
+        return map { String(format: "%02x", $0) }
+            .joined(separator: "")
+    }
+}
