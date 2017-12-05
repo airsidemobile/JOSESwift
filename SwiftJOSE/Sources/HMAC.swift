763145e3be0a66e38c7a60cc0ad2b8cd8e0e357d
Carol Capek <carol.capek@mohemian.com>
2017-12-05 17:37:56 +0100

    [JOSE-81] Do not throw in hmac method

--- SwiftJOSE/Sources/HMAC.swift
+++ SwiftJOSE/Sources/HMAC.swift
@@ -45,7 +45,7 @@ public struct HMAC {
      
      - Returns: The calculated HMAC.
      */
-    public static func calculate(from input: Data, with key: Data, using algorithm: HMACAlgorithm) throws -> Data {
+    public static func calculate(from input: Data, with key: Data, using algorithm: HMACAlgorithm) -> Data {
         var hmacOutData = Data(count: algorithm.outputLength)
         
         hmacOutData.withUnsafeMutableBytes { hmacOutBytes in
