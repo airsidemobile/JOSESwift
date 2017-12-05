8ddab95c8f5630088b440d597872d46cee0f955a
Carol Capek <carol.capek@mohemian.com>
2017-12-05 13:58:22 +0100

    [JOSE-81] Delete authentication

--- SwiftJOSE/Sources/HMAC.swift
+++ SwiftJOSE/Sources/HMAC.swift
@@ -53,29 +53,4 @@ public struct HMAC {
         
         return hmacOutData
     }
-    
-    /**
-     Checks if the reference authentication tag matches with, from the input calculated, authentication tag.
-     - Parameters:
-        - input: The input to authenticate a HMAC for.
-        - referenceAuthenticationTag: The reference authentication tag received with the message.
-        - key: The key used in the HMAC algorithm.
-        - algorithm: The algorithm used to calculate the HMAC.
-     
-     - Returns: True if the message is authenticated (the authentication tags match), false if the message is not authenticated (the authentication tags do not match)
-     */
-    public static func authenticate(input: Data, for referenceAuthenticationTag: Data, and key: Data, using algorithm: HMACAlgorithm) -> Bool {
-        let keyLength = size_t(kCCKeySizeAES256)
-        var hmacOutData = Data(count: 64)
-        
-        hmacOutData.withUnsafeMutableBytes { hmacOutBytes in
-            key.withUnsafeBytes { keyBytes in
-                input.withUnsafeBytes { inputBytes in
-                    CCHmac(algorithm.ccAlgorithm, keyBytes, keyLength, inputBytes, input.count, hmacOutBytes)
-                }
-            }
-        }
-        
-        return referenceAuthenticationTag == hmacOutData.subdata(in: 0..<32) ? true : false
-    }
 }
