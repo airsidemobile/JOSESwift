3e7404b2e2430834a42ee983fb4e02eb5be42abd
Carol Capek <carol.capek@mohemian.com>
2017-12-05 17:35:39 +0100

    [JOSE-81] Add HMAC tests

---
+++ Tests/HMACTests.swift
@@ -0,0 +1,19 @@
+//
+//  HMACTests.swift
+//  Tests
+//
+//  Created by Carol Capek on 05.12.17.
+//
+
+import XCTest
+
+class HMACTests: CryptoTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+}
