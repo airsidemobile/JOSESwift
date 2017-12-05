f3105c2e57855af709c9e5dcbd8c30d00a545a56
Carol Capek <carol.capek@mohemian.com>
2017-12-05 17:58:52 +0100

    [JOSE-81] Add HMAC tests

--- Tests/HMACTests.swift
+++ Tests/HMACTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+@testable import SwiftJOSE
 
 class HMACTests: CryptoTestCase {
     
@@ -15,5 +16,35 @@ class HMACTests: CryptoTestCase {
     
     override func tearDown() {
         super.tearDown()
+    }
+    
+    func testHMACCalculation() {
+        let key = "0102030405060708090a0b0c0d0e0f10111213141516171819".hexadecimalToData()!
+        let data = "cdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcd".hexadecimalToData()!
+        let hmacTestOutput = "b0ba465637458c6990e5a8c5f61d4af7e576d97ff94b872de76f8050361ee3dba91ca5c11aa25eb4d679275cc5788063a5f19741120c4f2de2adebeb10a298dd".hexadecimalToData()!
+        
+        let hmacOutput = HMAC.calculate(from: data, with: key, using: .SHA512)
+        
+        XCTAssertEqual(hmacOutput, hmacTestOutput)
+    }
+    
+    func testHMACCalculationWithFalseKey() {
+        let key = "1102030405060708090a0b0c0d0e0f10111213141516171819".hexadecimalToData()!
+        let data = "cdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcd".hexadecimalToData()!
+        let hmacTestOutput = "b0ba465637458c6990e5a8c5f61d4af7e576d97ff94b872de76f8050361ee3dba91ca5c11aa25eb4d679275cc5788063a5f19741120c4f2de2adebeb10a298dd".hexadecimalToData()!
+        
+        let hmacOutput = HMAC.calculate(from: data, with: key, using: .SHA512)
+        
+        XCTAssertNotEqual(hmacOutput, hmacTestOutput)
+    }
+    
+    func testHMACCalculationWithFalseData() {
+        let key = "0102030405060708090a0b0c0d0e0f10111213141516171819".hexadecimalToData()!
+        let data = "cdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcd".hexadecimalToData()!
+        let hmacTestOutput = "b0ba465637458c6990e5a8c5f61d4af7e576d97ff94b872de76f8050361ee3dba91ca5c11aa25eb4d679275cc5788063a5f19741120c4f2de2adebeb10a298dd".hexadecimalToData()!
+        
+        let hmacOutput = HMAC.calculate(from: data, with: key, using: .SHA512)
+        
+        XCTAssertNotEqual(hmacOutput, hmacTestOutput)
     }
 }
