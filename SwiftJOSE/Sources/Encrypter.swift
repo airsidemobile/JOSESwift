25757bc6e76d1e634211a049707fb3186bb7d9b5
Carol Capek <carol.capek@mohemian.com>
2017-12-06 17:52:39 +0100

    [JOSE-62] Add key generation error

--- SwiftJOSE/Sources/Encrypter.swift
+++ SwiftJOSE/Sources/Encrypter.swift
@@ -15,6 +15,7 @@ public enum EncryptionError: Error, Equatable {
     case cipherTextLenghtNotSatisfied
     case keyLengthNotSatisfied
     case hmacNotAuthenticated
+    case keyGenerationFailed(with: OSStatus)
     case encryptingFailed(description: String)
     case decryptingFailed(description: String)
 
@@ -114,7 +115,7 @@ internal protocol AsymmetricEncrypter {
 }
 
 internal protocol SymmetricEncrypter {
-    func randomCEK(for algorithm: SymmetricEncryptionAlgorithm) -> Data
+    func randomCEK(for algorithm: SymmetricEncryptionAlgorithm) throws -> Data
     func randomIV(for algorithm: SymmetricEncryptionAlgorithm) -> Data
 
     /**
@@ -167,7 +168,7 @@ public struct Encrypter {
             throw EncryptionError.encryptionAlgorithmNotSupported
         }
 
-        let cek = symmetric.randomCEK(for: enc)
+        let cek = try symmetric.randomCEK(for: enc)
         let encryptedKey = try asymmetric.encrypt(cek, using: alg)
         let symmetricContext = try symmetric.encrypt(payload.data(), with: cek, using: enc, additionalAuthenticatedData: header.data())
 
