71cc3907820235121dfd4a986eabf174d1a7b551
Carol Capek <carol.capek@mohemian.com>
2017-12-07 11:39:58 +0100

    [JOSE-62] Add random generation method and error

--- SwiftJOSE/Sources/Random.swift
+++ SwiftJOSE/Sources/Random.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+public enum RandomGenerationError: Error {
+    case failed(status: OSStatus)
+}
+
+public struct Random {
+    public static func generate(count: Int) throws -> Data {
+        var generatedRandom = Data(count: count)
+        
+        let randomGenerationStatus = generatedRandom.withUnsafeMutableBytes { mutableRandomBytes in
+            SecRandomCopyBytes(kSecRandomDefault, generatedRandom.count, mutableRandomBytes)
+        }
+        
+        guard randomGenerationStatus == errSecSuccess else {
+            throw RandomGenerationError.failed(status: randomGenerationStatus)
+        }
+        
+        return generatedRandom
+    }
+}
