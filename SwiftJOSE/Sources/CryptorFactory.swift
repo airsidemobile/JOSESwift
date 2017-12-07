03bccfc19a96955fd9c4d72f8c7a079f7336cbce
Daniel <daniel.egger@mohemian.com>
2017-12-07 10:15:39 +0100

    [JOSE-58] Add comments to cryptor factory

--- SwiftJOSE/Sources/CryptorFactory.swift
+++ SwiftJOSE/Sources/CryptorFactory.swift
@@ -7,8 +7,21 @@
 
 import Foundation
 
+/**
+  Factory deciding which cryptor to use for which algorithm.
+  If we had different cryptor versions e.g. for different platforms,
+  we could decide on which version to use here.
+ */
 struct CryptorFactory {
     
+    /**
+     Returns an asymmetric encrypter suitable for a given algorithm, initialized with a given public key.
+     - Parameters:
+        - algorithm: The asymmetric algorithm to use.
+        - publicKey: The public key to initialize the asymmetric encrypter with.
+     
+     - Returns: The asymmetric encrypter suitable for the given algorithm, initialized with the given public key.
+    */
     static func encrypter(for algorithm: AsymmetricEncryptionAlgorithm, with publicKey: SecKey) -> AsymmetricEncrypter {
         switch algorithm {
         case .RSAPKCS:
@@ -16,6 +29,13 @@ struct CryptorFactory {
         }
     }
     
+    /**
+     Returns an symmetric encrypter suitable for a given algorithm.
+     - Parameters:
+     - algorithm: The symmetric algorithm to use.
+     
+     - Returns: The symmetric encrypter suitable for the given algorithm.
+     */
     static func encrypter(for algorithm: SymmetricEncryptionAlgorithm) -> SymmetricEncrypter {
         switch algorithm {
         case .AESGCM256:
@@ -23,6 +43,14 @@ struct CryptorFactory {
         }
     }
     
+    /**
+     Returns an asymmetric decrypter suitable for a given algorithm, initialized with a given private key.
+     - Parameters:
+     - algorithm: The asymmetric algorithm to use.
+     - publicKey: The private key to initialize the asymmetric decrypter with.
+     
+     - Returns: The asymmetric decrypter suitable for the given algorithm, initialized with the given private key.
+     */
     static func decrypter(for algorithm: AsymmetricEncryptionAlgorithm, with privateKey: SecKey) -> AsymmetricDecrypter {
         switch algorithm {
         case .RSAPKCS:
@@ -30,6 +58,13 @@ struct CryptorFactory {
         }
     }
     
+    /**
+     Returns an symmetric decrypter suitable for a given algorithm.
+     - Parameters:
+     - algorithm: The symmetric algorithm to use.
+     
+     - Returns: The symmetric decrypter suitable for the given algorithm.
+     */
     static func decrypter(for algotithm: SymmetricEncryptionAlgorithm) -> SymmetricDecrypter {
         switch algotithm {
         case .AESGCM256:
