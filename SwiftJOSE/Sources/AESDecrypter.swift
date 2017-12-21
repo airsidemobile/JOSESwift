1cba5fecffe9f69e1197fb5d6ca53fb5954b60ff
Carol Capek <carol.capek@mohemian.com>
2017-12-21 11:34:07 +0100

    [JOSE-90] Delete 👾

--- SwiftJOSE/Sources/AESDecrypter.swift
+++ SwiftJOSE/Sources/AESDecrypter.swift
@@ -30,7 +30,7 @@ public struct AESDecrypter: SymmetricDecrypter {
         concatData.append(context.initializationVector)
         concatData.append(context.ciphertext)
         concatData.append(context.additionalAuthenticatedData.getByteLengthAsOctetHexData())
-        
+
         // Calculate the HMAC for the concatenated input data and compare it with the reference authentication tag.
         let hmacOutput = HMAC.calculate(from: concatData, with: hmacKey, using: algorithm.ccAlgorithms.hmacAlgorithm)
 
