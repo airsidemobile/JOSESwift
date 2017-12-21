76ec832ef90cf6aff6cb62c34d873e2e543b88b5
Carol Capek <carol.capek@mohemian.com>
2017-12-21 11:24:13 +0100

    [JOSE-90] Change comments and add init with compact serialized data

--- SwiftJOSE/Sources/JWE.swift
+++ SwiftJOSE/Sources/JWE.swift
@@ -56,14 +56,34 @@ public struct JWE {
         self.initializationVector = encryptionContext.initializationVector
         self.authenticationTag = encryptionContext.authenticationTag
     }
-
-    /// Initializes a JWE from a given compact serialization.
+    
+    /// Constructs a JWE object from a given compact serialization string.
+    ///
+    /// - Parameter compactSerialization: A compact serialized JWS object as string as received e.g. from the server.
+    /// - Throws:
+    ///     - `DeserializationError.invalidCompactSerializationComponentCount(count: Int)`: If the component count of the compact serialization is wrong.
+    ///     - `DeserializationError.componentNotValidBase64URL(component: String)`: If the component is not a valid base64URL string.
+    ///     - `DeserializationError.componentCouldNotBeInitializedFromData(data: Data)`: If a component cannot be initialized from its data object.
     public init(compactSerialization: String) throws {
         self = try JOSEDeserializer().deserialize(JWE.self, fromCompactSerialization: compactSerialization)
     }
-    
-    
 
+    /// Constructs a JWE object from a given compact serialization data.
+    ///
+    /// - Parameter compactSerialization: A compact serialized JWS object as data object as received e.g. from the server.
+    /// - Throws:
+    ///     - `DeserializationError.wrongDataEncoding(data: Data)`: If the compact serialization data object is not convertible to string.
+    ///     - `DeserializationError.invalidCompactSerializationComponentCount(count: Int)`: If the component count of the compact serialization is wrong.
+    ///     - `DeserializationError.componentNotValidBase64URL(component: String)`: If the component is not a valid base64URL string.
+    ///     - `DeserializationError.componentCouldNotBeInitializedFromData(data: Data)`: If a component cannot be initialized from its data object.
+    public init(compactSerialization: Data) throws {
+        guard let compactSerializationString = String(data: compactSerialization, encoding: .utf8) else {
+            throw DeserializationError.wrongDataEncoding(data: compactSerialization)
+        }
+        
+        self = try JOSEDeserializer().deserialize(JWE.self, fromCompactSerialization: compactSerializationString)
+    }
+    
     /// Initializes a JWE by providing all of it's five parts. Onyl used during deserialization.
     private init(header: JWEHeader, encryptedKey: Data, initializationVector: Data, ciphertext: Data, authenticationTag: Data) {
         self.header = header
