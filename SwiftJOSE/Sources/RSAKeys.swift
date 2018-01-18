2a94f372f18723516a90c1d867371bb3892fc4ed
Daniel <daniel.egger@mohemian.com>
2018-01-18 15:40:18 +0100

    [JOSE-94] Update access levels

--- SwiftJOSE/Sources/RSAKeys.swift
+++ SwiftJOSE/Sources/RSAKeys.swift
@@ -23,7 +23,7 @@
 
 import Foundation
 
-fileprivate enum RSAParameterName: String {
+private enum RSAParameterName: String {
     case modulus = "n"
     case exponent = "e"
     case privateExponent = "d"
