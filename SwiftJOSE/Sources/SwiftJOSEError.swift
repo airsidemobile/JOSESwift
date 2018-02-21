667af06737775d7afa49d87ad9426f54c173dfbd
Carol Capek <carol.capek@mohemian.com>
2018-02-21 16:04:58 +0100

    [JOSE-74] Add SwiftJOSEErrors

--- SwiftJOSE/Sources/SwiftJOSEError.swift
+++ SwiftJOSE/Sources/SwiftJOSEError.swift
@@ -8,5 +8,12 @@
 import Foundation
 
 public enum SwiftJOSEError: Error {
-    
+    case signingFailed(description: String)
+    case verifyingFailed(description: String)
+    case encryptingFailed(description: String)
+    case decryptingFailed(description: String)
+    case wrongDataEncoding(data: Data)
+    case invalidCompactSerializationComponentCount(count: Int)
+    case componentNotValidBase64URL(component: String)
+    case componentCouldNotBeInitializedFromData(data: Data)
 }
