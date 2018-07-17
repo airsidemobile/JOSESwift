8803df56bf5b7f789722044664ff4ae5dcc5d57b
Daniel <daniel.egger@mohemian.com>
2018-07-17 09:09:33 +0200

    [87] Add basic test suite for direct encryption

--- Tests/JWEDirectEncryptionTests.swift
+++ Tests/JWEDirectEncryptionTests.swift
@@ -8,37 +8,61 @@
 import XCTest
 @testable import JOSESwift
 
-class JWEDirectEncryptionTests: XCTestCase {
+class JWEDirectEncryptionTests: CryptoTestCase {
+
+    let data = "So Secret! 🔥🌵".data(using: .utf8)!
+
+    let serializationFromNimbus = """
+            eyJlbmMiOiJBMjU2Q0JDLUhTNTEyIiwiYWxnIjoiZGlyIn0..HUTNQ9m2Z8Q77tQJhLs5gg.DWQCCkrCPFeZ2-65L9__z83N1exh4oVIk4r\
+            OO2_v1eE.8sOW54Soupo_-TdXg5A9qXvokaHzS8cGb__ca3MvuEo
+            """
+
+    let keyFromNimbus = Data(bytes: [
+        177, 119,  33,  13, 164,  30, 108, 121,
+        207, 136, 107, 242,  12, 224,  19, 226,
+        198, 134,  17,  71, 173,  75,  42,  61,
+         48, 162, 206, 161,  97, 108, 185, 234,
+         60, 181,  90,  85,  51, 123,   6, 224,
+          4, 122,  29, 230, 151,  12, 244, 127,
+        121,  25,   4,  85, 220, 144, 215, 110,
+        130,  17,  68, 228, 129, 138,   7, 130
+    ])
     
-    func testExample() {
+    func testRoundtrip() {
         let symmetricKey = try! SecureRandom.generate(count: SymmetricKeyAlgorithm.A256CBCHS512.keyLength)
 
         let header = JWEHeader(algorithm: .direct, encryptionAlgorithm: .A256CBCHS512)
-
-        let payload = Payload("So Secret! 🔥🌵".data(using: .utf8)!)
-
+        let payload = Payload(data)
         let encrypter = Encrypter(keyEncryptionAlgorithm: .direct, encryptionKey: symmetricKey, contentEncyptionAlgorithm: .A256CBCHS512)!
 
         let jwe = try! JWE(header: header, payload: payload, encrypter: encrypter)
-
         let serialization = jwe.compactSerializedString
 
-        let j = try! JWE(compactSerialization: serialization)
-        let p = try! j.decrypt(with: symmetricKey)
-        let message = String(data: p.data(), encoding: .utf8)!
-
-        print(message)
+        try! XCTAssertEqual(JWE(compactSerialization: serialization).decrypt(with: symmetricKey).data(), data)
     }
 
-    func testExampleDecrypt() {
-        let symmetricKey = Data(base64URLEncoded: "sXchDaQebHnPiGvyDOAT4saGEUetSyo9MKLOoWFsueo8tVpVM3sG4AR6HeaXDPR_eRkEVdyQ126CEUTkgYoHgg")!
+    func testDecryptFromNimbus() {
+        let symmetricKey = keyFromNimbus
 
-        let jwe = try! JWE(compactSerialization: "eyJlbmMiOiJBMjU2Q0JDLUhTNTEyIiwiYWxnIjoiZGlyIn0..oIVW1sX2UKUm2WSRizxaKg.AS0v0ctVUYwHW2nTLAatC7j4lTIzw4OtE5MFM4ahCmA.B0MQ8Hh2Miv3tq5QCmtk6xROPezHZDSvylLsDnuNKTQ")
+        let jwe = try! JWE(compactSerialization: serializationFromNimbus)
 
-        let p = try! jwe.decrypt(with: symmetricKey)
-        let message = String(data: p.data(), encoding: .utf8)
+        try! XCTAssertEqual(jwe.decrypt(with: symmetricKey).data(), data)
+    }
 
-        print(message!)
+    func testDecryptWithWrongSymmetricKey() {
+        let symmetricKey = try! SecureRandom.generate(count: SymmetricKeyAlgorithm.A256CBCHS512.keyLength)
+
+        let jwe = try! JWE(compactSerialization: serializationFromNimbus)
+
+        XCTAssertThrowsError(try jwe.decrypt(with: symmetricKey))
+    }
+
+    func testDecryptWithWrongKeyType() {
+        let symmetricKey = privateKey2048!
+
+        let jwe = try! JWE(compactSerialization: serializationFromNimbus)
+
+        XCTAssertThrowsError(try jwe.decrypt(with: symmetricKey))
     }
     
 }
