368bfa3bf14b9be5802489656ef6940eda0ba67f
Felizia Bernutz <felizia.bernutz@gmx.de>
2018-10-08 09:50:55 +0200

    Add setters for additional JOSEHeader parameters (#110)
    * Add default argument for keyIdentifier
    
    * Update comment
    
    * Add test with optional keyIdentifier
    
    * Update documentation
    
    * Revert the changes in initializer
    
    * Add setter for JWS and JWE header parameters
    
    * Update test for setting header parameters
    
    * Add explicit type declaration to fix test on CI
    
    As the tests run successful locally, I assume there are some issues with the type
    
    * Fix formatting
    
    * Use xcode 10 on CI
    
    * Change type of x5c to array of String
    
    Like defined here: https://tools.ietf.org/html/rfc7515#section-4.1.6
    
    * Save type String instead of URL
    
    Use String when saving in internal dictionary for x5u and jku.
    This has the advantage of being JSON encodable.
    
    * Add test when setting non required parameters for JWSHeader
    
    * Add tests for JWE and JWS
    
    Roundtrip tests for encrypting/decrypting, signing/verifying
    with non required header parameters
    
    * Check for valid json before force try to serialize
    
    When it is not a valid json the header data doesn't get updated but there is no error thrown

--- JOSESwift/Sources/JWSHeader.swift
+++ JOSESwift/Sources/JWSHeader.swift
@@ -25,8 +25,17 @@ import Foundation
 
 /// The header of a `JWS` object.
 public struct JWSHeader: JOSEHeader {
-    let headerData: Data
-    let parameters: [String: Any]
+    var headerData: Data
+    var parameters: [String: Any] {
+        didSet {
+            guard JSONSerialization.isValidJSONObject(parameters) else {
+                return
+            }
+            // Forcing the try is ok here, because it is valid JSON.
+            // swiftlint:disable:next force_try
+            headerData = try! JSONSerialization.data(withJSONObject: parameters, options: [])
+        }
+    }
 
     /// Initializes a JWS header with given parameters and their original `Data` representation.
     /// Note that this (base64-url decoded) `Data` representation has to be exactly as it was
@@ -80,55 +89,111 @@ extension JWSHeader: CommonHeaderParameterSpace {
     /// The JWK Set URL which refers to a resource for a set of JSON-encoded public keys,
     /// one of which corresponds to the key used to sign the JWS.
     public var jku: URL? {
-        return parameters["jku"] as? URL
+        set {
+            parameters["jku"] = newValue?.absoluteString
+        }
+        get {
+            guard let parameter = parameters["jku"] as? String else {
+                return nil
+            }
+            return URL(string: parameter)
+        }
     }
 
     /// The JSON Web key corresponding to the key used to digitally sign the JWS.
     public var jwk: String? {
-        return parameters["jwk"] as? String
+        set {
+            parameters["jwk"] = newValue
+        }
+        get {
+            return parameters["jwk"] as? String
+        }
     }
 
     /// The Key ID indicates the key which was used to secure the JWS.
     public var kid: String? {
-        return parameters["kid"] as? String
+        set {
+            parameters["kid"] = newValue
+        }
+        get {
+            return parameters["kid"] as? String
+        }
     }
 
     /// The X.509 URL that referes to a resource for the X.509 public key certificate
     /// or certificate chain corresponding to the key used to sign the JWS.
     public var x5u: URL? {
-        return parameters["x5u"] as? URL
+        set {
+            parameters["x5u"] = newValue?.absoluteString
+        }
+        get {
+            guard let parameter = parameters["x5u"] as? String else {
+                return nil
+            }
+            return URL(string: parameter)
+        }
     }
 
     /// The X.509 certificate chain contains the X.509 public key certificate or
     /// certificate chain corresponding to the key used to sign the JWS.
-    public var x5c: [String: Any]? {
-        return parameters["x5c"] as? [String: Any]
+    public var x5c: [String]? {
+        set {
+            parameters["x5c"] = newValue
+        }
+        get {
+            return parameters["x5c"] as? [String]
+        }
     }
 
     /// The X.509 certificate SHA-1 thumbprint of the DER encoding of the X.509 certificate
     /// corresponding to the key used to sign the JWS.
     public var x5t: String? {
-        return parameters["x5t"] as? String
+        set {
+            parameters["x5t"] = newValue
+        }
+        get {
+            return parameters["x5t"] as? String
+        }
     }
 
     /// The X.509 certificate SHA-256 thumbprint of the DER encoding of the X.509 certificate
     /// corresponding to the key used to sign the JWS.
     public var x5tS256: String? {
-        return parameters["jwk"] as? String
+        set {
+            parameters["x5tS256"] = newValue
+        }
+        get {
+            return parameters["x5tS256"] as? String
+        }
     }
 
     /// The type to declare the media type of the JWS object.
     public var typ: String? {
-        return parameters["typ"] as? String
+        set {
+            parameters["typ"] = newValue
+        }
+        get {
+            return parameters["typ"] as? String
+        }
     }
 
     /// The content type to declare the media type of the secured content (payload).
     public var cty: String? {
-        return parameters["cty"] as? String
+        set {
+            parameters["cty"] = newValue
+        }
+        get {
+            return parameters["cty"] as? String
+        }
     }
 
     /// The critical header parameter indicates the header parameter extensions.
     public var crit: [String]? {
-        return parameters["crit"] as? [String]
+        set {
+            parameters["crit"] = newValue
+        }
+        get {
+            return parameters["crit"] as? [String]
+        }
     }
 }
