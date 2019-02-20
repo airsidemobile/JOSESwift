575d31ac8bef729ae54a0eccaecf75498c7a0475
Daniel <daniel.egger@mohemian.com>
2019-02-20 18:20:06 +0100

    Add single loop decoding for ec private key as well

--- JOSESwift/Sources/ECKeyCodable.swift
+++ JOSESwift/Sources/ECKeyCodable.swift
@@ -144,12 +144,15 @@ extension ECPrivateKey: Decodable {
         // Other common parameters are optional.
         var parameters: [String: JWKParameterType] = [:]
 
-        for key in commonParameters.allKeys where key.type == String.self{
-            parameters[key.rawValue] = try commonParameters.decode(String.self, forKey: key)
-        }
-
-        for key in commonParameters.allKeys where key.type == [String].self{
-            parameters[key.rawValue] = try commonParameters.decode([String].self, forKey: key)
+        for key in commonParameters.allKeys {
+            switch key.type {
+            case is String.Type:
+                parameters[key.rawValue] = try commonParameters.decode(String.self, forKey: key)
+            case is [String].Type:
+                parameters[key.rawValue] = try commonParameters.decode([String].self, forKey: key)
+            default:
+                break
+            }
         }
 
         // EC private key specific parameters.
