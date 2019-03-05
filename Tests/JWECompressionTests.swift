f0a595cc0756676e915cd7cc8dc56a1b08a76c3c
Florian <florian@haeser.it>
2019-03-05 14:35:46 +0100

    Fix trailing whitespace

--- Tests/JWECompressionTests.swift
+++ Tests/JWECompressionTests.swift
@@ -9,80 +9,80 @@ import XCTest
 @testable import JOSESwift
 
 class JWECompressionTests: RSACryptoTestCase {
-    
+
     let data = "So Secret! 🔥🌵".data(using: .utf8)!
     let compressedDataBase64URLEncodedString = "C85XCE5NLkotUVT4MH_K0g_ze7YCAA"
-    
+
     let jweSerializedNotSupportedZipHeaderValue = "eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIiwiemlwIjoiR1pJUCJ9..I62WKaitlHoJ-Kz3zvQ-Tw.Tu9hk_AMRMRTc8ggsoWifFS979Nz8xt-xx4FpF6waeE.w7c8eAGXpUD3tNskLzdl17s4vsCCSUwe5bRFpJg1kUs"
-    
+
     @available(*, deprecated)
     func testRoundtripWithLegacyDecrypter() {
         let symmetricKey = try! SecureRandom.generate(count: SymmetricKeyAlgorithm.A256CBCHS512.keyLength)
-        
+
         var header = JWEHeader(algorithm: .direct, encryptionAlgorithm: .A256CBCHS512)
         header.zip = "DEF"
-        
+
         let payload = Payload(data)
         let encrypter = Encrypter(keyEncryptionAlgorithm: .direct, encryptionKey: symmetricKey, contentEncyptionAlgorithm: .A256CBCHS512)!
-        
+
         let jwe = try! JWE(header: header, payload: payload, encrypter: encrypter)
         let serialization = jwe.compactSerializedString
-        
+
         try! XCTAssertEqual(JWE(compactSerialization: serialization).decrypt(with: symmetricKey).data(), data)
     }
-    
+
     func testRoundtrip() {
         let symmetricKey = try! SecureRandom.generate(count: SymmetricKeyAlgorithm.A256CBCHS512.keyLength)
-        
+
         var header = JWEHeader(algorithm: .direct, encryptionAlgorithm: .A256CBCHS512)
         header.zip = "DEF"
-        
+
         let payload = Payload(data)
         let encrypter = Encrypter(keyEncryptionAlgorithm: .direct, encryptionKey: symmetricKey, contentEncyptionAlgorithm: .A256CBCHS512)!
-        
+
         let jwe = try! JWE(header: header, payload: payload, encrypter: encrypter)
         let serialization = jwe.compactSerializedString
-        
+
         let decrypter = Decrypter(keyDecryptionAlgorithm: .direct, decryptionKey: symmetricKey, contentDecryptionAlgorithm: .A256CBCHS512)!
-        
+
         try! XCTAssertEqual(JWE(compactSerialization: serialization).decrypt(using: decrypter).data(), data)
     }
 
     // Note this test only works as long as the compression factory is invoked before the acutal decryption
     func testDecryptWithNotSupportedZipHeaderValue() {
         let symmetricKey = try! SecureRandom.generate(count: SymmetricKeyAlgorithm.A256CBCHS512.keyLength)
-        
+
         let jwe = try! JWE(compactSerialization: jweSerializedNotSupportedZipHeaderValue)
         let decrypter = Decrypter(keyDecryptionAlgorithm: .direct, decryptionKey: symmetricKey, contentDecryptionAlgorithm: .A256CBCHS512)!
         try XCTAssertThrowsError(jwe.decrypt(using: decrypter)) { error in
             XCTAssertEqual(error as! JOSESwiftError, JOSESwiftError.compressionAlgorithmNotSupported)
         }
     }
-    
+
     // Note this test only works as long as the compression factory is invoked before the acutal encryption
     func testEncryptWithNotSupportedZipHeaderValue() {
         let symmetricKey = try! SecureRandom.generate(count: SymmetricKeyAlgorithm.A256CBCHS512.keyLength)
-        
+
         var header = JWEHeader(algorithm: .direct, encryptionAlgorithm: .A256CBCHS512)
         header.zip = "GZIP"
-        
+
         let payload = Payload(data)
         let encrypter = Encrypter(keyEncryptionAlgorithm: .direct, encryptionKey: symmetricKey, contentEncyptionAlgorithm: .A256CBCHS512)!
         try XCTAssertThrowsError(JWE(header: header, payload: payload, encrypter: encrypter)) { error in
             XCTAssertEqual(error as! JOSESwiftError, JOSESwiftError.compressionAlgorithmNotSupported)
         }
     }
-    
+
     func testCompressorFactory() throws {
         let deflateCompressor = try CompressorFactory.makeCompressor(algorithm: CompressionAlgorithm.DEFLATE)
         XCTAssert(deflateCompressor is DeflateCompressor)
-        
+
         let noneCompressor = try CompressorFactory.makeCompressor(algorithm: CompressionAlgorithm.NONE)
         XCTAssert(noneCompressor is NoneCompressor)
-        
+
         try XCTAssertThrowsError(CompressorFactory.makeCompressor(algorithm: nil))
     }
-    
+
     func testNoneCompressor() throws {
         let noneCompressor = try CompressorFactory.makeCompressor(algorithm: CompressionAlgorithm.NONE)
         XCTAssert(noneCompressor is NoneCompressor)
@@ -95,10 +95,10 @@ class JWECompressionTests: RSACryptoTestCase {
     func testDeflateCompressor() throws {
         let deflateCompressor = try CompressorFactory.makeCompressor(algorithm: CompressionAlgorithm.DEFLATE)
         XCTAssert(deflateCompressor is DeflateCompressor)
-        
+
         var compressedData = try deflateCompressor.compress(data: data)
         XCTAssertEqual(compressedData.base64URLEncodedString(), compressedDataBase64URLEncodedString)
-        
+
         compressedData = Data.init(base64URLEncoded: compressedDataBase64URLEncodedString)!
         XCTAssertEqual(try deflateCompressor.decompress(data: compressedData), data)
     }
