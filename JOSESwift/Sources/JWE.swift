060044d17ddd87f68bb52d7d1ee2ccdc0085735d
Florian <florian@haeser.it>
2019-03-07 11:24:42 +0100

    [107] Add JWE compression support (#145)
    * Add CompressionAlgorithm enum
    
    * Add optional "zip" JWEHeader
    
    * Add test for optional zip JWE header
    
    * Add deflate/inflate protocol
    
    * Add Apple deflate compression wrapper
    
    * Add compression handling before encryption and before decryption
    
    * Add test cases for the compression feature
    
    * Add note on supported compression algorithm
    
    * Add compression handling before encryption and before decryption
    
    * Remove unnecessary protocol
    
    * Remove empty lines
    
    * Add none compressionAlgorithm, refactor string comparison
    
    * Add CompressorProtocol
    
    * Undo force unwrap
    
    * Rename factory method
    
    * Rename enum values
    
    * Rename enum values capitalized
    
    * Add explanatory comments to the Compressor file
    
    * Remove not needed check
    
    * Rename DeflateCompressor
    
    * Update JOSESwift/Sources/CompressionImplementation/Compressor.swift
    
    Co-Authored-By: fhaeser <florian@haeser.it>
    
    * Update JOSESwift/Sources/CompressionImplementation/Compressor.swift
    
    Co-Authored-By: fhaeser <florian@haeser.it>
    
    * Update JOSESwift/Sources/CompressionImplementation/Compressor.swift
    
    Co-Authored-By: fhaeser <florian@haeser.it>
    
    * Update README.md
    
    Co-Authored-By: fhaeser <florian@haeser.it>
    
    * Move compressor into appropriate group
    
    * Remove initializer for CompressionAlgorithm
    
    * Move zip header parameter into appropriate section
    
    * Properly name NONE
    
    * Add compression/decompression errors
    
    * Throw error instead of empty data
    
    * Default to NONE for not set zip header
    
    * Make use of NoneCompressor explicit
    
    * Add tests for compressor factory
    
    * Make encryptingFailed equatable
    
    * Properly assert unsupported compression algorithm error
    
    * Move compression into Payload
    
    * Add none and deflate compressor tests
    
    * Update JOSESwift/Sources/JWEHeader.swift
    
    Co-Authored-By: fhaeser <florian@haeser.it>
    
    * Update JOSESwift/Sources/JWEHeader.swift
    
    Co-Authored-By: fhaeser <florian@haeser.it>
    
    * Add compressionAlgorithm getter tests
    
    * Catch JOSESwiftError explicilty
    
    * Remove trailing whitespace
    
    * Fix trailing whitespace
    
    * Ignore force cast for test
    
    * Fix trailing whitespace...
    
    * Catch specific JOSESwiftErrors
    
    * Update CHANGELOG.md

--- JOSESwift/Sources/JWE.swift
+++ JOSESwift/Sources/JWE.swift
@@ -75,7 +75,11 @@ public struct JWE {
 
         var encryptionContext: EncryptionContext
         do {
-            encryptionContext = try encrypter.encrypt(header: header, payload: payload)
+            encryptionContext = try encrypter.encrypt(header: header, payload: payload.compressed(using: header.compressionAlgorithm))
+        } catch JOSESwiftError.compressionFailed {
+            throw JOSESwiftError.compressionFailed
+        } catch JOSESwiftError.compressionAlgorithmNotSupported {
+            throw JOSESwiftError.compressionAlgorithmNotSupported
         } catch {
             throw JOSESwiftError.encryptingFailed(description: error.localizedDescription)
         }
@@ -156,7 +160,13 @@ public struct JWE {
         }
 
         do {
-            return Payload(try decrypter.decrypt(context))
+            let decryptedData = try decrypter.decrypt(context)
+            let compressor = try CompressorFactory.makeCompressor(algorithm: header.compressionAlgorithm)
+            return Payload(try compressor.decompress(data: decryptedData))
+        } catch JOSESwiftError.decompressionFailed {
+            throw JOSESwiftError.decompressionFailed
+        } catch JOSESwiftError.compressionAlgorithmNotSupported {
+            throw JOSESwiftError.compressionAlgorithmNotSupported
         } catch {
             throw JOSESwiftError.decryptingFailed(description: error.localizedDescription)
         }
@@ -185,7 +195,13 @@ public struct JWE {
         }
 
         do {
-            return Payload(try decrypter.decrypt(context))
+            let compressor = try CompressorFactory.makeCompressor(algorithm: header.compressionAlgorithm)
+            let decryptedData = try decrypter.decrypt(context)
+            return Payload(try compressor.decompress(data: decryptedData))
+        } catch JOSESwiftError.decompressionFailed {
+            throw JOSESwiftError.decompressionFailed
+        } catch JOSESwiftError.compressionAlgorithmNotSupported {
+            throw JOSESwiftError.compressionAlgorithmNotSupported
         } catch {
             throw JOSESwiftError.decryptingFailed(description: error.localizedDescription)
         }
