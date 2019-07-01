13b7b3abb43d6ff01ab43140bec1a5b5189dd940
Daniel <daniel-mohemian@users.noreply.github.com>
2019-07-01 10:49:28 +0200

    Add simple Sonarqube setup (#158)
    * Update gitignore
    
    * Add conversion script and sonar scanner to brewfile
    
    * Add sonar config file and sonarqube lane
    
    * Update travis config
    
    * Dont run sonarqube in travis, only on release
    
    * Update fastlane/Fastfile
    
    Co-Authored-By: Markus Bürgler <markus.buergler@mohemian.com>

---
+++ scripts/xccov-to-sonarqube.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# See
+# https://docs.sonarqube.org/display/PLUG/Swift+Coverage+Results+Import
+# https://github.com/SonarSource/sonar-scanning-examples/blob/master/swift-coverage/swift-coverage-example/xccov-to-sonarqube-generic.sh
+
+function convert_file {
+  local xccovarchive_file="$1"
+  local file_name="$2"
+  echo "  <file path=\"$file_name\">"
+  xcrun xccov view --file "$file_name" "$xccovarchive_file" | \
+    sed -n '
+    s/^ *\([0-9][0-9]*\): 0.*$/    <lineToCover lineNumber="\1" covered="false"\/>/p;
+    s/^ *\([0-9][0-9]*\): [1-9].*$/    <lineToCover lineNumber="\1" covered="true"\/>/p
+    '
+  echo '  </file>'
+}
+
+function xccov_to_generic {
+  echo '<coverage version="1">'
+  for xccovarchive_file in "$@"; do
+    xcrun xccov view --file-list "$xccovarchive_file" | while read -r file_name; do
+      convert_file "$xccovarchive_file" "$file_name"
+    done
+  done
+  echo '</coverage>'
+}
+
+xccov_to_generic "$@"
