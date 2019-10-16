86c4580411e764dd7e31dc91b58df12b946c7f0a
Daniel <daniel.egger@mohemian.com>
2019-10-16 14:11:51 +0200

    Add missing quotes

--- scripts/update-license-year.sh
+++ scripts/update-license-year.sh
@@ -4,5 +4,5 @@ current_license_header="Copyright $(date +%Y) Airside Mobile Inc."
 any_license_header="Copyright [0-9]* Airside Mobile Inc."
 
 find JOSESwift/Sources Tests -name "*.swift" \
-	| xargs grep -lv $current_license_header \
+	| xargs grep -lv "$current_license_header" \
 	| xargs sed -i "s/$any_license_header/$current_license_header/"
