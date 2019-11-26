4606d3c40c309cf075dd808aadd06069df6b696f
Daniel <daniel.egger@mohemian.com>
2019-11-26 12:45:23 +0100

    Use xcresult files and update conversion script

--- scripts/xccov-to-sonarqube.sh
+++ scripts/xccov-to-sonarqube.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# See
+# Adapted from
 # https://docs.sonarqube.org/display/PLUG/Swift+Coverage+Results+Import
 # https://github.com/SonarSource/sonar-scanning-examples/blob/master/swift-coverage/swift-coverage-example/xccov-to-sonarqube-generic.sh
 
 function convert_file {
-  local xccovarchive_file="$1"
+  local xcresult_bundle="$1"
   local file_name="$2"
   echo "  <file path=\"$file_name\">"
-  xcrun xccov view --file "$file_name" "$xccovarchive_file" | \
+  xcrun xccov view --archive --file "$file_name" "$xcresult_bundle" | \
     sed -n '
     s/^ *\([0-9][0-9]*\): 0.*$/    <lineToCover lineNumber="\1" covered="false"\/>/p;
     s/^ *\([0-9][0-9]*\): [1-9].*$/    <lineToCover lineNumber="\1" covered="true"\/>/p
@@ -19,9 +19,9 @@ function convert_file {
 
 function xccov_to_generic {
   echo '<coverage version="1">'
-  for xccovarchive_file in "$@"; do
-    xcrun xccov view --file-list "$xccovarchive_file" | while read -r file_name; do
-      convert_file "$xccovarchive_file" "$file_name"
+  for xcresult_bundle in "$@"; do
+    xcrun xccov view --archive --file-list "$xcresult_bundle" | while read -r file_name; do
+      convert_file "$xcresult_bundle" "$file_name"
     done
   done
   echo '</coverage>'
