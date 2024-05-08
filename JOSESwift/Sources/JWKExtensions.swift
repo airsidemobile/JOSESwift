ee1d22976b8e09770042ca1feb52c2e5e451377b
Daniel <daniel@mohemian.com>
2024-05-08 17:12:17 +0200

    Make json encoding of jwks deterministic

--- JOSESwift/Sources/JWKExtensions.swift
+++ JOSESwift/Sources/JWKExtensions.swift
@@ -35,7 +35,7 @@ public extension JWK {
 
 public extension JWK {
     func jsonString() -> String? {
-        guard let json = try? JSONEncoder().encode(self) else {
+        guard let json = jsonData() else {
             return nil
         }
 
@@ -43,6 +43,8 @@ public extension JWK {
     }
 
     func jsonData() -> Data? {
-        return try? JSONEncoder().encode(self)
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .sortedKeys
+        return try? encoder.encode(self)
     }
 }
